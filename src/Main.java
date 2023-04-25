94eb95dee8a35820499a7c220182fe8cbaf61856
sumithanvat <skhanvat@gmail.com>
2023-04-26 00:26:26 +0530

    Added:Find max in 3 Stringt value

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ public class Main {
      */
     public static void main(String[] args) {
         // Test cases
-        // Test case 1: max number is at 1st position
+        // Test case 1: max word is at 1st position
             System.out.println("Test case 1: max number is at 1st position");
-            System.out.println(new MaxNumber<Float>().findMax(70.3f, 65.7f, 71.2f)); // expected output: 70.3f
+            System.out.println(new MaxNumber<String>().findMax("c","a","b")); // expected output: c
 
-            // Test case 2: max number is at 2nd position
+            // Test case 2: max word is at 2nd position
             System.out.println("Test case 2: max number is at 2nd position");
-            System.out.println(new MaxNumber<Float>().findMax(11.7f, 12.9f, 9.3f)); // expected output: 12.9f
+            System.out.println(new MaxNumber<String>().findMax("atul","prafull","akku")); // expected output: prafull
 
-            // Test case 3: max number is at 3rd position
+            // Test case 3: max word is at 3rd position
             System.out.println("Test case 3: max number is at 3rd position");
-            System.out.println(new MaxNumber<Float>().findMax(77.3f, 35.4f, 87.3f)); // expected output: 87.3f
+            System.out.println(new MaxNumber<String>().findMax("apple","banana","mango")); // expected output: mango
         }
     }
