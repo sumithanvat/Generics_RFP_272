509e94f38d3efc642a610eb69493d81f83a4c5a3
sumithanvat <skhanvat@gmail.com>
2023-04-26 23:10:51 +0530

    Added:program extend to take more than 3 parameter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import com.bridgelabz.MaxNumber;
+
 import com.bridgelabz.MaximumFinder;
+
 
 public class Main {
     /*
@@ -7,34 +8,13 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        // Integer Test Cases
-        MaximumFinder<Integer> intMaxFinder1 = new MaximumFinder<>(7, 3, 5);
-        System.out.println("Max Integer: " + intMaxFinder1.testMaximum());
+        int a = 1, b = 2, c = 3,d = 4;
+        System.out.println(MaximumFinder.max(a, b, c, d)); // Output: 4
 
-        MaximumFinder<Integer> intMaxFinder2 = new MaximumFinder<>(10, 15, 8);
-        System.out.println("Max Integer: " + intMaxFinder2.testMaximum());
+        double v = 0.7,x = 1.1, y = 2.2, z = 3.3;
+        System.out.println(MaximumFinder.max(v,x, y, z)); // Output: 3.3
 
-        MaximumFinder<Integer> intMaxFinder3 = new MaximumFinder<>(20, 18, 25);
-        System.out.println("Max Integer: " + intMaxFinder3.testMaximum());
-
-        // Float Test Cases
-        MaximumFinder<Float> floatMaxFinder1 = new MaximumFinder<>(3.5f, 7.2f, 5.1f);
-        System.out.println("Max Float: " + floatMaxFinder1.testMaximum());
-
-        MaximumFinder<Float> floatMaxFinder2 = new MaximumFinder<>(10.5f, 15.3f, 8.7f);
-        System.out.println("Max Float: " + floatMaxFinder2.testMaximum());
-
-        MaximumFinder<Float> floatMaxFinder3 = new MaximumFinder<>(20.1f, 18.6f, 25.9f);
-        System.out.println("Max Float: " + floatMaxFinder3.testMaximum());
-
-        // String Test Cases
-        MaximumFinder<String> strMaxFinder1 = new MaximumFinder<>("apple", "peach", "banana");
-        System.out.println("Max String: " + strMaxFinder1.testMaximum());
-
-        MaximumFinder<String> strMaxFinder2 = new MaximumFinder<>("dog", "cat", "fish");
-        System.out.println("Max String: " + strMaxFinder2.testMaximum());
-
-        MaximumFinder<String> strMaxFinder3 = new MaximumFinder<>("orange", "lemon", "grape");
-        System.out.println("Max String: " + strMaxFinder3.testMaximum());
+        String s1 = "apple", s2 = "banana", s3 = "orange", s4 = "papaya";
+        System.out.println(MaximumFinder.max(s1, s2, s3,s4)); // Output: papaya
     }
 }
--- src/com/bridgelabz/MaximumFinder.java
+++ src/com/bridgelabz/MaximumFinder.java
@@ -1,32 +1,23 @@
 package com.bridgelabz;
 
+import java.util.*;
+
+
 public class MaximumFinder<T extends Comparable<T>> {
 
-
-    private T x;
-    private T y;
-    private T z;
-
-    public MaximumFinder(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public static int max(int... values) {
+        Arrays.sort(values);
+        return values[values.length - 1];
     }
 
-    public T testMaximum() {
-        return MaximumFinder.findMaximum(x, y, z);
+    public static double max(double... values) {
+        Arrays.sort(values);
+        return values[values.length - 1];
     }
 
-    public static <T extends Comparable<T>> T findMaximum(T x, T y, T z) {
-        T max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
+    public static String max(String... values) {
+        Arrays.sort(values);
+        return values[values.length - 1];
     }
-
 
 }
